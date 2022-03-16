618a34fc71da9f25c9af81ea3bb5abe4155feb47
yxh <yxh1103@qq.com>
2022-03-16 18:03:33 +0800

    删除过多的错误提示

--- gftoken/utils.go
+++ gftoken/utils.go
@@ -1,8 +1,6 @@
 package gftoken
 
 import (
-	"github.com/gogf/gf/v2/errors/gerror"
-	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/net/ghttp"
 	"strings"
 )
@@ -14,25 +12,19 @@ type AuthFailed struct {
 	Message string `json:"message"`
 }
 
-func (m *GfToken) getRequestToken(r *ghttp.Request) (token string, err error) {
+func (m *GfToken) getRequestToken(r *ghttp.Request) (token string) {
 	authHeader := r.Header.Get("Authorization")
 	if authHeader != "" {
 		parts := strings.SplitN(authHeader, " ", 2)
 		if !(len(parts) == 2 && parts[0] == "Bearer") {
-			g.Log().Error(r.GetCtx(), "authHeader:"+authHeader+" get Token key fail")
-			err = gerror.New("get Token key fail")
 			return
 		} else if parts[1] == "" {
-			g.Log().Error(r.GetCtx(), "authHeader:"+authHeader+" get Token fail")
-			err = gerror.New(" get Token fail")
 			return
 		}
 		token = parts[1]
 	} else {
 		authHeader = r.Get("token").String()
 		if authHeader == "" {
-			g.Log().Error(r.GetCtx(), "params:"+authHeader+" get Token key fail")
-			err = gerror.New("query Token fail")
 			return
 		}
 		token = authHeader
@@ -41,12 +33,7 @@ func (m *GfToken) getRequestToken(r *ghttp.Request) (token string, err error) {
 }
 
 func (m *GfToken) GetToken(r *ghttp.Request) (tData *tokenData, err error) {
-	var token string
-	token, err = m.getRequestToken(r)
-	if err != nil {
-		g.Log().Errorf(r.GetCtx(), "ParseToken error: %s\n", err.Error())
-		return
-	}
+	token := m.getRequestToken(r)
 	tData, _, err = m.getTokenData(r.GetCtx(), token)
 	return
 }
@@ -58,15 +45,7 @@ func (m *GfToken) IsLogin(r *ghttp.Request) (b bool, failed *AuthFailed) {
 		// 如果不需要认证，继续
 		return
 	}
-	token, err := m.getRequestToken(r)
-	if err != nil {
-		b = false
-		failed = &AuthFailed{
-			Code:    FailedAuthCode,
-			Message: err.Error(),
-		}
-		return
-	}
+	token := m.getRequestToken(r)
 	if m.IsEffective(r.GetCtx(), token) == false {
 		b = false
 		failed = &AuthFailed{
