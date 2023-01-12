623980dafdd9fa3e364547bec14807023b9d29f8
yxh <yxh1103@qq.com>
2023-01-12 15:39:31 +0800

    升级 gf版本至v2.3.0

--- gftoken/options.go
+++ gftoken/options.go
@@ -1,6 +1,7 @@
 package gftoken
 
 import (
+	_ "github.com/gogf/gf/contrib/nosql/redis/v2"
 	"github.com/gogf/gf/v2/database/gredis"
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/os/gcache"
